f6b50ae7ead470e9d692ce487f5402cc434bd468
vrun545 <vrun545@gmail.com>
2024-05-21 12:32:39 +0530

    Added code for logging

--- conftest.py
+++ conftest.py
@@ -7,7 +7,30 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+import logging
 
+# Function to configure logging
+def configure_logging():
+    logs_dir = Path("logs")
+    logs_dir.mkdir(parents=True, exist_ok=True)
+    log_file_path = logs_dir / "test.log"
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S %p',
+        handlers=[
+            logging.FileHandler(log_file_path, mode='w')
+        ]
+    )
+    logging.getLogger("wdm").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
+
+# Call the configure_logging function to set up logging
+configure_logging()
+
+
+# Fixture
 @pytest.fixture(scope="class", params=["firefox", "chrome", "headless"], autouse=True)
 def browserSetup(request):
     if request.param == "firefox":
@@ -27,6 +50,7 @@ def browserSetup(request):
     yield
     driver.quit()
 
+# setup for html-report
 @pytest.hookimpl(tryfirst=True)
 def pytest_configure(config):
     today = datetime.now()
@@ -36,5 +60,11 @@ def pytest_configure(config):
     config.option.htmlpath = pytest_html
     config.option.self_contained_html = True
 
+# setup for title of report
 def pytest_html_report_title(report):
     report.title = "WebShop Test Report"
+
+# Hook function to log additional information when a test fails
+def pytest_runtest_makereport(item, call):
+    if call.when == "call" and call.excinfo is not None:
+        logging.error(f"Test case {item.name} failed. Details: {call.excinfo}")
