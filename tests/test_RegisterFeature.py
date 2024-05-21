c1ed1a8667bb632b3fc60703cf0f4e4d9bcb7413
vrun545 <vrun545@gmail.com>
2024-05-21 12:31:25 +0530

    Added logs for each test case

--- tests/test_RegisterFeature.py
+++ tests/test_RegisterFeature.py
@@ -1,11 +1,11 @@
 from pytest import mark
 from selenium.common import TimeoutException
+import logging
 
 from conftest import *
 from pages.LoginPage import LoginPage
 import configparser
 import os
-
 from pages.RegisterPage import RegisterPage
 
 # Path to the config.ini file relative to the root folder
@@ -25,10 +25,10 @@ password = config['Registration']['password']
 BaseUrl = config['Settings']['base_url']
 productName = config['SearchBox']['productName']
 
-
 @pytest.mark.usefixtures("browserSetup")
 @pytest.mark.order(1)
 class Test_Register:
+
     # Launching Website and Maximize Window Size
     def setup_class(self):
         self.driver.get(BaseUrl+"register")
@@ -38,16 +38,20 @@ class Test_Register:
     # Test Case for Registering as New User
     @pytest.mark.register
     def test_registration(self):
+        logging.info("Starting registration test...")
         status = self.register_page.register(firstname, lastname, email, password)
         # Assertion
         assert status, "User registration failed !!!"
+        logging.info("Registration test completed successfully.")
 
     @pytest.mark.searchproduct
     # Test Case for Searching a Product on Search-Box
     def test_searchProduct(self):
+        logging.info("Starting product search test...")
         status = self.register_page.searchProduct(productName)
         # Assertion
         assert status, "Product search failed !!!"
+        logging.info("Product search test completed successfully.")
 
     # Closing Driver
     def teardown_class(self):
